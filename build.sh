6ac726a03568f2d81f4562cfd44c1b669929c363
Archies Arck <archies@expertrec.com>
2021-12-03 15:22:19 +0530

    Added build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+# need version number as parameter
+
+version_number="3.0.4"
+
+script_path=$(readlink -f $(dirname $0))
+temp_folder_name="/tmp/release"
+
+cd ${script_path}
+rm -rf ${temp_folder_name}
+mkdir -p ${temp_folder_name}/Expertrecsitesearch
+cp -r ${script_path}/* ${temp_folder_name}/Expertrecsitesearch/
+cd ${temp_folder_name}/Expertrecsitesearch/
+rm build.sh README.md
+
+sed -i 's/0.0.0/'${version_number}'/g' etc/module.xml
+sed -i 's/0.0.0/'${version_number}'/g' composer.json
+
+cd ${temp_folder_name}
+zip -r expertrec_expertrecsitesearch-${version_number}.zip ${temp_folder_name}/Expertrecsitesearch
+
+rm -rf Expertrecsitesearch
+mv * ${script_path}
