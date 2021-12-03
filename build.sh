47757920bf9c3778968551bb401051eff1e88434
Archies Arck <archies@expertrec.com>
2021-12-03 15:38:14 +0530

    Working script

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ temp_folder_name="/tmp/release"
 cd ${script_path}
 rm -rf ${temp_folder_name}
 mkdir -p ${temp_folder_name}/Expertrecsitesearch
-cp -r ${script_path}/* ${temp_folder_name}/Expertrecsitesearch/
+cp -r $(ls) ${temp_folder_name}/Expertrecsitesearch/
 cd ${temp_folder_name}/Expertrecsitesearch/
 rm build.sh README.md
 
@@ -17,7 +17,7 @@ sed -i 's/0.0.0/'${version_number}'/g' etc/module.xml
 sed -i 's/0.0.0/'${version_number}'/g' composer.json
 
 cd ${temp_folder_name}
-zip -r expertrec_expertrecsitesearch-${version_number}.zip ${temp_folder_name}/Expertrecsitesearch
+zip -r expertrec_expertrecsitesearch-${version_number}.zip Expertrecsitesearch
 
 rm -rf Expertrecsitesearch
 mv * ${script_path}
