c91acb759f66d4de024a002f87763ef8c24e7986
Archies Arck <archies@expertrec.com>
2021-12-09 15:25:57 +0530

    Adding upgrade schema script to test whether fixture generation fails

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,26 @@
+<?php
+namespace Expertrec\ExpertrecSiteSearch\Setup;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Expertrec\ExpertrecSiteSearch\Helper\Data;
+
+class UpgradeData implements UpgradeDataInterface{
+    protected $helperData;
+    public function __construct(Data $helperData){
+        $this->helperData = $helperData;
+    }
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context){
+        $setup->startSetup();
+        try{
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $entityType = $objectManager->get('Magento\Eav\Model\Config')->getEntityType('catalog_product');
+            $this->helperData->sendFullSync();
+        }
+        catch(\Magento\Framework\Exception\LocalizedException $e){
+            $this->logger->Info("Expertrec: in Upgrade Data: catalog_product entity not found");
+        }
+        $setup->endSetup();
+    }
+}
