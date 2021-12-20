ee444b3a70b7eec13f938c623db1160b748798b7
Archies Arck <archies@expertrec.com>
2021-12-20 16:31:23 +0530

    Added logger in reindex script when called by dashboard

--- Controller/Adminhtml/Reindex/Fullsyncdata.php
+++ Controller/Adminhtml/Reindex/Fullsyncdata.php
@@ -9,7 +9,7 @@ class Fullsyncdata extends \Magento\Backend\App\Action
     protected $resultPageFactory;
     protected $jsonHelper;
     protected $helper;
-
+    protected $logger;
     /**
      * Constructor
      *
@@ -20,11 +20,13 @@ class Fullsyncdata extends \Magento\Backend\App\Action
         \Magento\Backend\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
         \Magento\Framework\Json\Helper\Data $jsonHelper,
-        \Expertrec\ExpertrecSiteSearch\Helper\Data $helper
+        \Expertrec\ExpertrecSiteSearch\Helper\Data $helper,
+        \Psr\Log\LoggerInterface $logger
     ) {
         $this->resultPageFactory = $resultPageFactory;
         $this->jsonHelper = $jsonHelper;
         $this->helper = $helper;
+        $this->logger = $logger;
         parent::__construct($context);
     }
 
@@ -36,11 +38,27 @@ class Fullsyncdata extends \Magento\Backend\App\Action
     public function execute()
     {
         try {
+            $this->logger->info("Expertrec: Reindex started from dashboard");
             $this->helper->sendFullSync();
+            $this->helper->log_to_endpoint('{
+                "location":"[CRON] FullSync.php",
+                "data":"calling sendFullSync() from CRON"
+            }');
             return $this->jsonResponse('Reindex Started');
         } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            $this->helper->log_to_endpoint('{
+                "location":"[CRON] FullSync.php",
+                "data":"Exception in calling sendFullSync() from CRON: ' . $e->getMessage() . '",
+                "trace":"' . $e->getTraceAsString() . ' "
+            }');
             return $this->jsonResponse($e->getMessage());
         } catch (\Exception $e) {
+            $this->helper->log_to_endpoint('{
+                "location":"[CRON] FullSync.php",
+                "data":"Exception in calling sendFullSync() from CRON: ' . $e->getMessage() . '",
+                "trace":"' . $e->getTraceAsString() . ' "
+            }');
+            $this->logger->info("Exppertrec: error in reindex from dashboard: ");
             $this->logger->critical($e);
             return $this->jsonResponse($e->getMessage());
         }
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 # need version number as parameter
 
-version_number="3.0.5"
+version_number="3.0.6"
 
 script_path=$(readlink -f $(dirname $0))
 temp_folder_name="/tmp/release"
@@ -13,8 +13,8 @@ cp -r $(ls) ${temp_folder_name}/ExpertrecSiteSearch/
 cd ${temp_folder_name}/ExpertrecSiteSearch/
 rm build.sh README.md
 
-sed -i 's/3.0.5/'${version_number}'/g' etc/module.xml
-sed -i 's/3.0.5/'${version_number}'/g' composer.json
+sed -i 's/3.0.6/'${version_number}'/g' etc/module.xml
+sed -i 's/3.0.6/'${version_number}'/g' composer.json
 
 cd ${temp_folder_name}
 zip -r expertrec_expertrecsitesearch-${version_number}.zip ExpertrecSiteSearch
